01ed041da4416b0b087b018274f8b00c052eabde
Mr. M1M3 <abir.sheikh.4044@gmail.com>
2023-06-07 20:33:50 +0600

    :books: added documentation

--- src/lib/utils/validator.util.ts
+++ src/lib/utils/validator.util.ts
@@ -1,3 +1,16 @@
+/*
+ * validate data using any schema
+
+       ### Any data that needs to be validated should use this module ###
+       
+ * Usage
+       * Generic `Schema` is the typeof schema created by zod.
+        ### ATTENTION: Schema is the actual typeof schema you get from `typeof Schema`. 
+        ### ATTENTION: It's not what you get by using `z.infer<typeof Schema>`
+       * Argument operand is the data you want the validator to validate
+       * Argument schema is the actual zod schema
+       * It returns `operand` or `ZodError` object wrapped inside Result<T, E>
+*/
 import type { Result } from "$lib/types/Result.types";
 import type { z } from "zod";
 
