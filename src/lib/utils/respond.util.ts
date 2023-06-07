d5b5e6fc76e2cce2a6d0e95d2dbca27202f0b1ec
Mr. M1M3 <abir.sheikh.4044@gmail.com>
2023-06-07 13:35:55 +0600

    :zap: added utility function to generate response

---
+++ src/lib/utils/respond.util.ts
@@ -0,0 +1,31 @@
+/*
+ * Generate responses to respond
+
+       ### Any server only load function or form actions should use respond<T, Schema>(result: Result):Processed<T, Schema> to generate and return response ###
+       
+ * Usage
+       * Generic T is the shape of the actual data that was intended to respond
+       * Generic Schema is the schema of the user input which will be used to ensure type-safety if the input was invalid
+       * Argument result of type Result is used to determine whether it was a success or error
+       * As the return value of this function would be returned to the user, it returns data of type Processed<T, Scheme>;
+*/
+
+import generate_invalid_error_msg from "$lib/gens/invalid-error-msg.gen";
+import type { Processed } from "$lib/types/Processed.type";
+import type { Result } from "$lib/types/Result.types";
+import { error } from "@sveltejs/kit";
+import { ZodError } from "zod";
+
+export default function respond<T, Schema>(result: Result<T, Schema>): Processed<T, Schema>{
+    if(result.result === "success"){
+        return {
+            ok: true,
+            data: result.original
+        }
+    }
+    if(result.error instanceof ZodError){
+        return generate_invalid_error_msg<Schema>(result.error);
+    }
+
+    throw error(500, "Something went wrong on the server");
+}
