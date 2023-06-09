0b54a11927c1381eb5a106967de7f1ae0e9dcc03
Mr. M1M3 <abir.sheikh.4044@gmail.com>
2023-06-09 19:12:04 +0600

    :zap: logging in now sets cookie and redirects to /

--- src/lib/server/api/login.ts
+++ src/lib/server/api/login.ts
@@ -1,13 +1,15 @@
 import type { Result } from "$lib/types/Result.types";
 import Pocketbase from "pocketbase";
 
-export default async function login<Data>(credentials: {email: string, password: string}): Promise<Result<Data, unknown>>{
+export default async function login<Data>(credentials: {email: string, password: string}): Promise<Result<{session: string}, unknown>>{
     const pb = new Pocketbase('http://127.0.0.1:8090');
     try{
-        const authorized =  await pb.collection('students').authWithPassword<Data>(credentials.email, credentials.password);
+        await pb.collection('students').authWithPassword<Data>(credentials.email, credentials.password);
         return {
             result: "success",
-            original: structuredClone(authorized.record)
+            original: {
+                session: pb.authStore.exportToCookie()
+            }
         }
     }catch(e){
         return {
