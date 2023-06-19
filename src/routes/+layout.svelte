5486bdf824b05dbfee33ecb7e4df0ceff91959f8
Mr-M1M3 <abir.sheikh.4044@gmail.com>
2023-06-19 09:29:57 +0600

    :sparkles: setup skeleton ui

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import '@skeletonlabs/skeleton/themes/theme-gold-nouveau.css';
+  import "@skeletonlabs/skeleton/styles/skeleton.css";
+  import "../app.postcss";
+</script>
+
+<slot></slot>
